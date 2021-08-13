f27f9b4ae96b80a7073178e687b0b3b701a76be9
Bennet Ngan <bennet.ngan@gmail.com>
2021-08-13 18:00:49 -0400

    Added youtube embed/report link

--- src/Components/DownloadPage.js
+++ src/Components/DownloadPage.js
@@ -4,8 +4,20 @@ export const DownloadPage = (setFlag) => {
 
     return (
         <div className="App-header">
-            <h1>DownloadPage</h1>
-            <a download="test.txt" href="https://drive.google.com/file/d/1HuOFv4GxaCmyzb8yFjS06HXTejzuJmI9/view?usp=sharing">Test File</a>
+        <h1 class="title">Video Presentation</h1>
+
+        <iframe 
+             width="853" 
+             height="505" 
+             src="https://www.youtube.com/embed/k1f0A-v6q3o" 
+             title="YouTube video player" 
+             frameborder="0" 
+             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+             allowfullscreen>
+
+             </iframe>
+
+            <a href="https://docs.google.com/document/d/1f4OOFVQLsdlTFezGkjTG39a2PkEC4vcAJbJFRv8iR1o/edit?usp=sharing">Final Report</a>
         </div>
     )
 }
